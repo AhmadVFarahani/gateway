e4ae1067ac8c897273efc8ff4aa1b1de59cc8c4d
ahmad <vasheghani.f.a@gmail.com>
2025-04-27 22:14:23 -0700

    Add .gitignore and clean unnecessary files and Route Table

---
+++ Gateway.Domain/Entities/Route.cs
@@ -0,0 +1,14 @@
+﻿namespace Gateway.Domain.Entities;
+
+public class Route:BaseEntity
+{
+    public long Id { get; set; }
+    public string Path { get; set; } = string.Empty;
+    public string HttpMethod { get; set; } = string.Empty;
+    public string TargetPath { get; set; } = string.Empty;
+    public long ServiceId { get; set; }
+    public bool RequiresAuthentication { get; set; } = true;
+    public bool IsActive { get; set; } = true;
+  
+    public Service? Service { get; set; }
+}
--- Gateway.Domain/Entities/Service.cs
+++ Gateway.Domain/Entities/Service.cs
@@ -5,4 +5,6 @@ public class Service:BaseEntity
     public string Name { get; set; } = string.Empty;
     public string BaseUrl { get; set; } = string.Empty;
     public bool IsActive { get; set; } = true;
+
+    public ICollection<Route> Routes { get; set; } = new List<Route>();
 }
